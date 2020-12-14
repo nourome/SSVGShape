586fb1f5cb07b53915179653d358ff0861388333
nour <nour.saffaf@gmail.com>
2020-12-14 15:59:56 +0200

    implementation of SSVGShape struct

--- Sources/SSVGShape/SSVGShape.swift
+++ Sources/SSVGShape/SSVGShape.swift
@@ -1,3 +1,30 @@
-struct SSVGShape {
-    var text = "Hello, World!"
+
+
+import SwiftUI
+
+struct SSVGShape<R: SVGReader>: Shape {
+    
+    private let paths: [SVGPath]
+    let reader: R
+    
+    init(reader: R) {
+        self.reader = reader
+        let result = reader.parse()
+        
+        switch result {
+        case .success(let p):
+            paths = p
+        case .failure(let error):
+            fatalError("Error \(error.localizedDescription)")
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { p in
+            for path in paths {
+                path.draw(p: &p)
+            }
+        }
+    }
+    
 }
